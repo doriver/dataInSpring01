54b9945868a4febc6e140a4325132dbe1f3eb529
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2024-12-31 10:25:43 +0900

    이제 브랜치 분기할꺼임

--- src/main/java/com/example/data/entity/CrwlReply.java
+++ src/main/java/com/example/data/entity/CrwlReply.java
@@ -30,4 +30,7 @@ public class CrwlReply {
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime createdAt;
 
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false)
+    private LocalDateTime updatedAt; // insertable = false 안해주면 db에 null로 들어감, 아마 jpa가 필드 값을 명시적으로 설정하지 않으면 null로 처리해서 그런듯
+
 }
