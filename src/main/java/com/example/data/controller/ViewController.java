b801945d64c10163c53ef4c368a8504494430a48
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2025-01-08 12:36:30 +0900

    es에 있는 데이터 브라우저view에서 확인함

--- src/main/java/com/example/data/controller/ViewController.java
+++ src/main/java/com/example/data/controller/ViewController.java
@@ -1,12 +1,15 @@
 package com.example.data.controller;
 
 import com.example.data.entity.CrwlPost;
+import com.example.data.entity.es.PostEs;
 import com.example.data.repository.CrwlPostRepository;
+import com.example.data.repository.PostEsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -14,6 +17,17 @@ public class ViewController {
 
     @Autowired
     private CrwlPostRepository crwlPostRepository;
+    @Autowired private PostEsRepository postEsRepository;
+
+    @RequestMapping("/es/hh")
+    public String aaes(Model model) {
+
+        Iterable<PostEs> pesList = postEsRepository.findAll();
+
+        model.addAttribute("pesList", pesList);
+        return "hhhss";
+    }
+
 
     @RequestMapping("/hh")
     public String aa(Model model) {
@@ -22,4 +36,9 @@ public class ViewController {
         return "hhh";
     }
 
+    @RequestMapping("/vv")
+    public String vvaa() {
+
+        return "viewTest";
+    }
 }
